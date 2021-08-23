f9c27e483e41408876c01e25d7232fb7c83f727b
Taras Shapran <shaprantaras2000@gmail.com>
2021-08-23 11:06:18 +0300

    add Switch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import {
     BrowserRouter as Router,
+    Switch,
     Route,
     Link,
 } from "react-router-dom";
+
+
 import Cars from "./components/cars/Cars";
 import CreateCar from "./components/create-car/CreateCar";
 import UpdateCar from "./components/updateCar/UpdateCar";
@@ -17,9 +20,11 @@ function App() {
                 <Link to={'/cars'}>cars </Link>
                 <Link to={'/create-car'}>create-car</Link>
                 <Link to={'/update-car'}>update-car</Link>
+                <Switch>
                 <Route path={'/cars'} component={Cars}/>
                 <Route path={'/create-car'} component={CreateCar}/>
                 <Route path={'/update-car'} component={UpdateCar}/>
+                </Switch>
             </div>
         </Router>
     );
