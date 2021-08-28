e37c6a1d51a1f290ef9148d8112724396c647c5b
Taras Shapran <shaprantaras2000@gmail.com>
2021-08-28 14:03:30 +0300

    homework 7

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import {useReducer} from "react";
+
+const reduser = (state, action) => {
+    switch (action) {
+        case 'INCREASE_BY_TEN':
+            return state + 10;
+        case 'REDUCE_BY_TEN':
+            return state - 2;
+        default:
+            return state;
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
+    let [state, dispatch] = useReducer(reduser, 0);
 
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <h1>{state}</h1>
+            <button onClick={() => dispatch('INCREASE_BY_TEN')}>add ten</button>
+
+            <button onClick={() => dispatch('REDUCE_BY_TEN')}>sub two</button>
+        </div>
+    );
 }
 
 export default App;
